bb91368bcd707a7b29757811db86000893e3f046
munuprig <egorprygun@ya.ru>
2025-09-19 17:19:17 +0700

    спринт 11

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 public interface UserStorage {
     List<User> findAll();
+
     User create(User user);
+
     User update(User user);
+
     User findUserById(int id);
 }
