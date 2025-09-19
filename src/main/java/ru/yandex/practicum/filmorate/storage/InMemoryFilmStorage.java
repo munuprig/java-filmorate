9bfb442ed0681a9cee7d55f27d1a1f1d38e26123
munuprig <egorprygun@ya.ru>
2025-09-19 18:16:42 +0700

    спринт 11

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.storage;
 
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 
@@ -8,6 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Component
 public class InMemoryFilmStorage implements FilmStorage {
     private int idFilmGenerator = 0;
     private final Map<Integer, Film> films = new HashMap<>();
