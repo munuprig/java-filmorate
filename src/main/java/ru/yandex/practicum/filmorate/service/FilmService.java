fcb5a50bc8fb86c94a9da3f37837983528f73773
munuprig <egorprygun@ya.ru>
2025-09-23 16:10:13 +0700

    спринт 11 исправление 2, после понимания почему не делался build

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -9,7 +9,6 @@ import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
