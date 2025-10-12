8946d9541dc9f369fa9fb7bfc16529dd5921fb13
munuprig <egorprygun@ya.ru>
2025-10-12 13:10:37 +0700

    спринт 12 исправление 1

--- src/test/java/ru/yandex/practicum/filmorate/FilmValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmValidationTest.java
@@ -6,7 +6,6 @@ import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
